6ef9943ada0c83370e5355d5e7fee024f7e33fe4
olgierd <opilarczyk@gmail.com>
2013-11-20 18:42:01 +0100

    Added makefile and library for reading files

--- recorder/LMRecorder.cpp
+++ recorder/LMRecorder.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include "Leap.h"
-#include <curses.h>
+#include <stdio.h>
+#include <time.h>
 
 #define czas_start()    clock_gettime(CLOCK_REALTIME, &t1)
-#define czas_stop()             clock_gettime(CLOCK_REALTIME, &t2)
+#define czas_stop()     clock_gettime(CLOCK_REALTIME, &t2)
 #define czas_oblicz()   czas = (double)(t2.tv_sec - t1.tv_sec) + 1.e-9*(t2.tv_nsec - t1.tv_nsec)
 
 using namespace std;
 using namespace Leap;
-
 
 
 class SampleListener : public Listener {
@@ -19,50 +19,59 @@ public:
 	Frame frame;
 	Finger finger;
 	int count, diffx, diffy;
-	ofstream outfile;
+	FILE *outfile;
 	int i;
     timespec t1, t2;
     double czas;
-
+    long lastTime;
 
 	SampleListener() {
 		count = 0;
 		diffx = 0;
 		diffy = 0;
+		lastTime = 0;
 	}
 
 	virtual void onConnect(const Controller&) {
-		cout << "initialized!" << endl;
-		initscr();
-		cbreak();
-		outfile.open("output.lmr");
-//		czas_start();
+		outfile = fopen("output.lmr", "w");
+		clock_gettime(CLOCK_REALTIME, &t1);
 	}
 
 	virtual void onFrame(const Controller& c) {
 
 		frame = c.frame();
 		finger = frame.fingers().leftmost();
-		mvaddch(140-(int)finger.tipPosition().y, 120+(int)finger.tipPosition().x, '#');
-		refresh();
 
-//		czas_oblicz();
-
-//		outfile << czas/1000 << "#";
-
-		for(i=0; i<frame.fingers().count(); i++) {
-			finger = frame.fingers()[i];
-			outfile << "#" << finger.tipPosition().x << ";" << finger.tipPosition().y << ";" << finger.tipPosition().z;
-		}
+		clock_gettime(CLOCK_REALTIME, &t2);
+		czas = (double)(t2.tv_sec - t1.tv_sec) + 1.e-9*(t2.tv_nsec - t1.tv_nsec);
+		czas *= 1000;
 
 		if(frame.fingers().count())
-			outfile << "\n";
+			fprintf(outfile, "%.4f", czas);
+
+		// add all fingers
+		for(i=0; i<frame.fingers().count(); i++) {
+			finger = frame.fingers()[i];
+			fprintf(outfile, "#%f;%f;%f;", finger.tipPosition().x, finger.tipPosition().y, finger.tipPosition().z);
+			fprintf(outfile, "%f;%f;%f", finger.direction().x, finger.direction().y, finger.direction().z);
+		}
+
+		// if there were any fingers, add newline (avoids empty lines)
+		if(frame.fingers().count())
+			fprintf(outfile, "\n");
+
+
+		printf("F: %d T: %dms FPS: %.2f FPS_AVG: %.2f\n", count, (int)(czas), 1000.0/(czas-lastTime), 1000*count/czas);
+
+		lastTime = czas;
+		count++;
+
 
 	}
 
 	virtual void onDisconnect(const Controller& c) {
 
-		outfile.close();
+		fclose(outfile);
 
 	}
 
@@ -73,13 +82,14 @@ int main() {
 	SampleListener listener;
 	Controller controller;
 
+	cout << "Press Enter key to start/stop recording!" << endl;
+	cin.get();
+
 	controller.addListener(listener);
 
-	std::cout << "Press Enter to quit..." << std::endl;
-	std::cin.get();
+	cin.get();
 
 	controller.removeListener(listener);
-	endwin();
 
 	return 0;
 
---
+++ recorder/test.cpp
@@ -0,0 +1,36 @@
+#include "LMReader.h"
+
+int main(int argc, char **argv) {
+
+	if(argc<2) {
+		printf("Usage: %s filename\n", argv[0]);
+		exit(1);
+	}
+
+	// create Reader object and read given file
+	LMReader *r = new LMReader(argv[1]);
+
+	// if there's a frame
+	while(r->nextFrame()) {
+
+		// get no. of fingers
+		printf("%d fingers: ", r->fingerCount);
+
+		// show position of fingertip
+		// getfinger(FINGERID, PARAM)
+		// FINGERID - id of finger in view - supports up to 20 fingers in field of view (is that enough?)
+		// PARAM - available are TIPX, TIPY, TIPZ for fingertip and DIRX, DIRY, DIRZ
+
+		for(int i=0; i<r->fingerCount; i++) {
+			printf("X: %.4f / Y: %.4f / Z: %.4f # ", r->getFinger(i, TIPX), r->getFinger(i, TIPY), r->getFinger(i, TIPZ));
+		}
+
+		printf("\n");
+
+	}
+
+	return 0;
+}
+
+
+
